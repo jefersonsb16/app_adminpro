a6a4cf557ca7477f5ca9d7b505251cd2e582459a
Jeferson Salazar <jeferson.salazar@sunsetswh.com>
2019-01-03 09:52:10 -0500

    Agregar modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+
+// Modulos
+import { SharedModule } from '../shared/shared.module';
+
+// Routes
+import { PAGES_ROUTES } from './pages.routes';
+
+// Components
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+export class PagesModule {}
